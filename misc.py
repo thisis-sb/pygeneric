9dc7b23076b0b45b3efdad30c797096e2d264b7d
Sunil Bhat <iamsunilbhat@gmail.com>
2022-12-29 08:50:17 +0530

    CHG_ID_00015

--- misc.py
+++ misc.py
@@ -3,6 +3,7 @@ Miscellaneous methods
 """
 
 import sys
+import pandas as pd
 
 def progress_str(n1, n2):
     msg = '    %d/%d Completed' % (n1, n2)
@@ -11,3 +12,10 @@ def progress_str(n1, n2):
 def print_progress_str(n1, n2, end=''):
     print(progress_str(n1, n2), end=end)
     sys.stdout.flush()
+
+def df2print_segrows(df, col):
+    ''' Silly but works '''
+    mask = df[col].ne(df[col].shift(-1))
+    df1 = pd.DataFrame('', index=mask.index[mask] + .5, columns=df.columns)
+    df = pd.concat([df, df1]).sort_index().reset_index(drop=True).iloc[:-1]
+    return df
