f3b317d09d60bbff677f62a169d92c43e690996c
Sunil Bhat <iamsunilbhat@gmail.com>
2023-09-04 18:48:22 +0530

    CHG_ID_00066

--- plot_utils.py
+++ plot_utils.py
@@ -13,11 +13,11 @@ def default_plot_settings(font_size=6, font_weight='bold'):
     plt.rcParams['lines.linewidth'] = 1
     return
 
-def format_plot(ax, title, ylim=None, style=None, yscale=None, x_interval=1):
+def format_plot(ax, title, ylim=None, style=None, yscale=None, x_interval=1, x_format='%b'):
     ax.xaxis.set_major_locator(mdates.MonthLocator(interval=x_interval))
     ax.xaxis.set_minor_locator(mdates.MonthLocator())
     ax.xaxis.set_major_formatter(mdates.ConciseDateFormatter(ax.xaxis.get_major_locator()))
-    ax.xaxis.set_major_formatter(mdates.DateFormatter('%b'))
+    ax.xaxis.set_major_formatter(mdates.DateFormatter(x_format))
     ax.tick_params(axis='both', which='major', labelsize=6)
     ax.set_title(title, fontsize=6, fontweight='bold')
     ax.grid(True)
