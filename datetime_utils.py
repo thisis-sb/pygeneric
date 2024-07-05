42b446cc953a8a2ffd5671a749f957a8e1d3b97d
thisis-sb <iamsunilbhat@gmail.com>
2024-07-05 19:56:46 +0530

    change 00116

--- datetime_utils.py
+++ datetime_utils.py
@@ -1,4 +1,5 @@
 import time
+from datetime import date, datetime
 
 _timers_dict = {}
 def _time_since_last(id, precision=0):
@@ -20,6 +21,31 @@ def elapsed_time(id, precision=2):
 
 def remove_timers(id):
     [_timers_dict.pop(x) for x in id] if type(id) == list else _timers_dict.pop(id)
+
+def last_n_pe_dates(n, last_period=None):
+    end_date_str = date.today().strftime('%Y-%m-%d') if last_period is None else last_period
+    pe_dates = []
+    for yr in range(2018, datetime.strptime(end_date_str, '%Y-%m-%d').year + 1):
+        for dt in ['%d-03-31' % yr, '%d-06-30' % yr, '%d-09-30' % yr, '%d-12-31' % yr]:
+            if dt <= end_date_str:
+                pe_dates.append(dt)
+    return pe_dates[-n:]
+
+def get_period_ends(**kwargs):
+    pe = kwargs['pe'] if 'pe' in kwargs.keys() else None
+    lp = kwargs['lp'] if 'lp' in kwargs.keys() else None
+    np = kwargs['np'] if 'np' in kwargs.keys() else None
+    verbose = kwargs['verbose'] if 'verbose' in kwargs.keys() else False
+    if verbose:
+        print('\nget_period_ends: kwargs:', kwargs, end=', ')
+    if pe is not None:
+        period_ends = pe
+    else:
+        last_period = lp if lp is not None else date.today().strftime('%Y-%m-%d')
+        period_ends = last_n_pe_dates(np, last_period=last_period)
+    if verbose:
+        print('period_ends: %s\n' % period_ends)
+    return period_ends
 
 ''' --------------------------------------------------------------------------------------- '''
 if __name__ == '__main__':
